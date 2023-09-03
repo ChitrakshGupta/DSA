19260ff0eae40ca215cf1b008bd3f1b639d5b139
ChitrakshG <chitrakshgupta@jklu.edu.in>
2023-09-03 23:41:47 +0530

    third commit

---
+++ twosum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b;
+cout << "enter number 1:"<<endl;
+cin >>a;
+cout << "enter number 2:"<<endl;
+cin >>b;
+cout <<"sum is :"<<a+b;
+
+
+    return 0;
+}
