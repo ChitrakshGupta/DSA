a90ef2eb3846c135393fb43cd8f7dfa5c28d8a1d
ChitrakshG <chitrakshgupta@jklu.edu.in>
2023-09-04 21:23:48 +0530

    04:09:2023

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void print(string name){
+    for (int i=0; i<5; i++){
+        cout<< name << endl;
+
+    }
+}
+int main(){
+    string name="chitraksh";
+    
+
+print(name);
+
+
+    return 0;
+}
