b782249a7d2f32c67ec9ad94dbdf5a7b272869d5
ChitrakshG <chitrakshgupta@jklu.edu.in>
2023-09-04 21:29:45 +0530

    04:09:2023

---
+++ fun_add_twono.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int add(int x, int y){
+    return x+y;
+}
+int main(){
+    int x=add(1,2);
+    cout<< "sum is "<< x<< endl;
+    
+
+
+
+    return 0;
+}
