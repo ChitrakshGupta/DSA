ccc2da398f03679488549d8067724953da984143
ChitrakshG <chitrakshgupta@jklu.edu.in>
2023-09-04 21:54:42 +0530

    04:09:2023

---
+++ ques1fun.cpp
@@ -0,0 +1,11 @@
+// write fun sum of three numbers
+# include <iostream>
+using namespace std;
+void add(int a,int b,int c){
+cout<< "sum is " <<a+b+c <<endl;
+}
+int main(){
+
+add(1,2,3);
+    return 0;
+}
