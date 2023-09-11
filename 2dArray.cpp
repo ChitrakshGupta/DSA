504096197d9768fc94a7462967c232925cdded7a
ChitrakshG <chitrakshgupta@jklu.edu.in>
2023-09-11 21:46:47 +0530

    11:09:2023

--- 2dArray.cpp
+++ 2dArray.cpp
@@ -10,21 +10,24 @@ cout<<"enter no of col :";
 cin>>c;
 
 cout<<"row "<<r<<"\ncol "<<c<<endl;
+// declaretion 
 int arr[r][c];
 
-
+//input all elements
 for(int i=0; i<r; i++){
     cout<<"enter "<<i<<"row "<<endl;
     for(int j=0; j<c; j++){
         cin>>arr[i][j];
     }
 }
+// printing all elements
 
 for(int i=0; i<r; i++){
-    cout<<"enter "<<i<<"row "<<endl;
+    // cout<<"enter "<<i<<"row "<<endl;
     for(int j=0; j<c; j++){
-        cout<<arr[i][j]<<endl;
+        cout<<arr[i][j]<<" ";
     }
+    cout<<"\n";
 }
     return 0;
 }
