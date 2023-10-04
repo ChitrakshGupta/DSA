84559fb0738728380cda2629ccf795c485c0a1e4
ChitrakshG <chitrakshgupta@jklu.edu.in>
2023-10-04 23:41:10 +0530

    04-10-2023

--- .vscode/Lecture_Pointer/pointer1.cpp
+++ .vscode/Lecture_Pointer/pointer1.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 int main()
 {
+    int a=5;
+    int *p=&a;
+    cout<<p<<endl;  // p is the address of a
+    cout<<*p<<endl; // *p is the value of a
+    cout<<a<<endl;    // a is the value of a
+
+    
+
+
     /* code */
     return 0;
 }
