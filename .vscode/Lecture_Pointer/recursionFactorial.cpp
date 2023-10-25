1ae7c1a062ca6d343379d6f6b19bff5559938d3e
ChitrakshG <chitrakshgupta@jklu.edu.in>
2023-10-26 00:58:40 +0530

    26-10-2023

--- .vscode/Lecture_Pointer/recursionFactorial.cpp
+++ .vscode/Lecture_Pointer/recursionFactorial.cpp
@@ -9,6 +9,13 @@ int fact(int n){
     int prevFact = fact(n-1);
     return n*prevFact;
 }
+int sum_of_digits(int n){
+    if(n==0){
+        return 0;
+    }
+    int prev_sum = sum_of_digits(n/10);
+    return n%10 + prev_sum;
+}
 int main(){
     int n;
     cin>>n;
